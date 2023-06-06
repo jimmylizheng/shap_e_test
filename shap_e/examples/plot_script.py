27ece4909ee5199ac04e0186b799536d9cc9ddf8
jimmylizheng <jimmyli@umich.edu>
2023-06-06 10:42:09 -0400

    add plot script

--- shap_e/examples/plot_script.py
+++ shap_e/examples/plot_script.py
@@ -15,7 +15,7 @@ def main():
                     temp_str2+='image300M, '
                 else:
                     temp_str2+='text300M, '
-                if w1=='dec':
+                if w2=='dec':
                     temp_str2+='decoder, '
                 else:
                     temp_str2+='transmitter, '
@@ -23,6 +23,7 @@ def main():
                 label_dict[temp_str1]=temp_str2
     # file_list=['base40M-textvec-out.json','base40M-out.json','base300M-out.json']
     # label_dict={'base40M-textvec-out.json':'base40M, text-only','base40M-out.json':'base40M','base300M-out.json':'base300M'}
+    plt.figure(figsize=(10,6))
     for file_name in file_list:
         # Read the contents of the file
         with open(file_name, "r") as file:
@@ -40,7 +41,7 @@ def main():
     plt.yticks(fontsize=12)
     plt.legend(fontsize=12,loc='lower right')
     plt.grid(True)
-    plt.savefig('./point-e-mem-plot.png')
+    plt.savefig('./shap-e-mem-plot.png')
     plt.show()
 
 if __name__ == "__main__":
